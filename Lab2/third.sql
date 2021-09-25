5f6c7a5309b5c28610ceec617ff75f985f4a56d1
Dayana Khairanova <d_khairanova@kbtu.kz>
2021-09-25 18:33:21 +0600

    lab2

--- Lab2/third.sql
+++ Lab2/third.sql
@@ -1,43 +1,22 @@
 CREATE TABLE students(
-    full_name varchar PRIMARY KEY ,
+    full_name varchar(30) PRIMARY KEY,
     age integer NOT NULL,
-    birt_date date,
+    birth_date date,
     gender varchar(7),
-    gpa float NOT NULL,
-    info text CHECK ( char_length(info) < 30 ),
+    averege_grade float NOT NULL,
+    info text CHECK ( char_length(info) < 50 ),
     dorm boolean,
-    ad_info text NOT NULL
+    adit_info text NOT NULL
 );
 
 CREATE TABLE instructors(
-    full_name varchar PRIMARY KEY ,
+    full_name varchar PRIMARY KEY,
+    languages varchar(20) NOT NULL,
     remote_lessons boolean NOT NULL
 );
 
-CREATE TABLE capabilities(
-    cap_id integer PRIMARY KEY ,
-    inst_id varchar references instructors(full_name)
-);
-
-CREATE TABLE jobs(
-    job_id integer PRIMARY KEY,
-    company_name varchar NOT NULL ,
-    cap_id integer references capabilities(cap_id)
-);
-
-CREATE TABLE languages(
-    lan_id integer PRIMARY KEY ,
-    lan_name varchar(10) NOT NULL ,
-    cap_id integer references capabilities(cap_id)
-);
-
-CREATE TABLE lesson_participants (
-    title varchar(15) NOT NULL PRIMARY KEY ,
-    instructor varchar references instructors(full_name),
-    room_number integer CHECK ( room_number > 0 )
-);
-
 CREATE TABLE lesson(
-    les_id varchar(15) references lesson_participants(title),
-    stud_id varchar references students(full_name)
+    instr_name varchar references instructors(full_name),
+    stud_name varchar references students(full_name),
+    r_number integer CHECK ( r_number > 0 )
 );
